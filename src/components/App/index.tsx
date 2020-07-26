dff1d2c76f62283a340b02cb94daf2a59e07c856
KiriZhu <kiribaty88@gmail.com>
2020-07-26 17:48:42 +0200

    create and style Header, NumberDisplay and Face components

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import NumberDisplay from '../NumberDisplay';
 import './App.scss';
 
 const App: React.FC = () => {
-  return <div className='App'> Hello from App </div>;
+  return (
+    <div className='App'>
+      <div className='Header'>
+        <NumberDisplay value={0} />
+        <div className='Face'>
+          <span role='img' aria-label='Face'>
+            😎
+          </span>
+        </div>
+        <NumberDisplay value={23} />
+      </div>
+      <div className='Body'>Body</div>
+    </div>
+  );
 };
 
 export default App;
